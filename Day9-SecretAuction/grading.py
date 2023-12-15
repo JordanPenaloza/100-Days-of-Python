eb822c3f7bbabd63f793b8c1974ab38921699d1e
JordanPenaloza <jordan332033@gmail.com>
2023-12-15 11:16:40 -0800

    grading challenge complete

---
+++ Day9-SecretAuction/grading.py
@@ -0,0 +1,26 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+    score = student_scores[student]
+    if score >= 91:
+        student_grades[student] = "Outstanding"
+    elif score >= 81:
+        student_grades[student] = "Exceeds Expectations"
+    elif score >= 71:
+        student_grades[student] = "Acceptable"
+    elif score <= 70:
+        student_grades[student] = "Fail"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
