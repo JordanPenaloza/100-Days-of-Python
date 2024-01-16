1d26a95f9e3b8f1cc9fbb2a739019b999956e502
JordanPenaloza <jordan332033@gmail.com>
2024-01-16 13:19:20 -0800

    finished game with bugs
    current bugs
    -player cards doesn't reset after ending the game
    -after ending the game user is prompted if they want to draw another card

--- Day11-Blackjack/main.py
+++ Day11-Blackjack/main.py
@@ -3,44 +3,108 @@ import random
 import os
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-player_cards = []
-bot_cards = []
+
 
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def play_game():
-    print(logo)
-    player_cards.append(draw_card())
-    player_cards.append(draw_card())
-    player_score = calculate_score(player_cards)
-    print(f"Your cards: {player_cards}, current score = {player_score}")
+    player_cards = []
+    bot_cards = []
 
+    player_cards.append(draw_card())
+    player_cards.append(draw_card())
     bot_cards.append(draw_card())
     bot_cards.append(draw_card())
-    bot_score = calculate_score(bot_cards)
-    bot_first_hand = bot_cards[0]
-    print(f"Bot's first hand: {bot_first_hand}")
-    
-    choice = 'y'   
-    while choice == 'y':
-        if player_score <= 21:
-            choice = input("Type 'y' to grab another card or 'n' to skip") 
-            player_cards.append(draw_card())
-            player_score = calculate_score(player_cards)
-            print(f"Your cards: {player_cards}, current score = {player_score}")
-            print(f"Bot's first hand: {bot_first_hand}")
-        else:
-            player_score = calculate_score(player_cards)
-            print(f"Your cards: {player_cards}, current score = {player_score}")
-            print(f"Bot's first hand: {bot_first_hand}")
-            print(f"Your final hand: {player_cards}, final score = {player_score}")
-            print(f"Bot's final hand: {bot_cards}, bot's final score = {bot_score}")
-            print("You went over, you lose")
-            choice == 'n'
+
+    playing = True
+    while playing:
+
+        print(logo)
+
+        player_score = calculate_score(player_cards)
+        #player_cards, player_score = check_cards(player_cards, player_score)
+        print(f"Your cards: {player_cards}, current score = {player_score}")
+
+        bot_score = calculate_score(bot_cards)
+        #bot_cards, bot_score = check_cards(bot_cards, bot_score)
+        bot_first_hand = bot_cards[0]
+        print(f"Bot's first hand: {bot_first_hand}")
+
+        player_has_blackjack = check_blackjack(player_cards)
+        bot_has_blackjack = check_blackjack(bot_cards)
+        if player_has_blackjack and not bot_has_blackjack:
+            print("Win! You have a blackjack!")
+            
+        elif bot_has_blackjack:
+            print("Lose! Bot has a blackjack")
+        
+        if check_ace(player_cards):
+            player_cards, player_score = check_cards(player_cards, player_score)
+        elif check_ace(bot_cards):
+            bot_cards, bot_score = check_cards(bot_cards, bot_score)
+        
+        while not check_gameover(player_score):
+            draw_again = input("Would you like to draw another card? (y/n): ")
+            if draw_again == 'y':
+                player_cards.append(draw_card())
+                print(f"Your cards: {player_cards}, current score = {player_score}")
+                print(f"Bot's first hand: {bot_first_hand}")
+            
+                if bot_score < 17:
+                    bot_cards.append(draw_card())
+            else: 
+                if bot_score < 17:
+                    bot_cards.append(draw_card())
+
+                print(f"Your final hand: {player_cards}, final score = {player_score}")
+                print(f"Bot final hand: {bot_cards}, bot final score = {bot_score}")
+                
+                if player_score > bot_score:
+                    print("You win!")
+                elif player_score == bot_score:
+                    print("Draw")
+                else:
+                    print("You lose!")
+            
+            if check_gameover(player_score):
+                print(f"Your final hand: {player_cards}, final score = {player_score}")
+                print(f"Bot final hand: {bot_cards}, bot final score = {bot_score}")
+                print("You went over, you lose!")
+
+            
         
 
+
+        
+
+def check_gameover(score):
+    if score > 21:
+        return True
+    else:
+        return False
+    
+def check_blackjack(cards):
+    if 11 in cards and 10 in cards:
+        return True
+    else:
+        return False
+
+def check_ace(cards):
+    if 11 in cards:
+        return True
+    else:
+        return False
+    
+def check_cards(cards, score):
+    for card in cards:
+        if card == 11:
+            if score > 21:
+                card = 1
+    score = calculate_score(cards)
+    return cards, score
+        
 def draw_card():
     card = cards[random.randint(0,12)]
     return card
