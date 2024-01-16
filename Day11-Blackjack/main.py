46600fcd0d0947f84174c34cb0cba83dcab4ee55
JordanPenaloza <jordan332033@gmail.com>
2024-01-16 13:46:30 -0800

    fixed all bugs
    bugs fixed:
    -game not ending when blackjack detected
    -still losing when the bot went over 21
    
    game complete!

--- Day11-Blackjack/main.py
+++ Day11-Blackjack/main.py
@@ -4,31 +4,28 @@ import os
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-
-
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def play_game():
-    player_cards = []
-    bot_cards = []
 
-    player_cards.append(draw_card())
-    player_cards.append(draw_card())
-    bot_cards.append(draw_card())
-    bot_cards.append(draw_card())
 
     playing = True
     while playing:
+        player_cards = []
+        bot_cards = []
+
+        player_cards.append(draw_card())
+        player_cards.append(draw_card())
+        bot_cards.append(draw_card())
+        bot_cards.append(draw_card())
 
         print(logo)
 
         player_score = calculate_score(player_cards)
-        #player_cards, player_score = check_cards(player_cards, player_score)
         print(f"Your cards: {player_cards}, current score = {player_score}")
 
         bot_score = calculate_score(bot_cards)
-        #bot_cards, bot_score = check_cards(bot_cards, bot_score)
         bot_first_hand = bot_cards[0]
         print(f"Bot's first hand: {bot_first_hand}")
 
@@ -36,9 +33,12 @@ def play_game():
         bot_has_blackjack = check_blackjack(bot_cards)
         if player_has_blackjack and not bot_has_blackjack:
             print("Win! You have a blackjack!")
+            playing = False
+
             
         elif bot_has_blackjack:
             print("Lose! Bot has a blackjack")
+            playing = False
         
         if check_ace(player_cards):
             player_cards, player_score = check_cards(player_cards, player_score)
@@ -46,18 +46,22 @@ def play_game():
             bot_cards, bot_score = check_cards(bot_cards, bot_score)
         
         game_over = False
-        while not game_over:
+        while playing and not game_over:
+
             draw_again = input("Would you like to draw another card? (y/n): ")
             if draw_again == 'y':
                 player_cards.append(draw_card())
+                player_score = calculate_score(player_cards)
                 print(f"Your cards: {player_cards}, current score = {player_score}")
                 print(f"Bot's first hand: {bot_first_hand}")
             
                 if bot_score < 17:
                     bot_cards.append(draw_card())
+                    bot_score = calculate_score(bot_cards)
             else: 
                 if bot_score < 17:
                     bot_cards.append(draw_card())
+                    bot_score = calculate_score(bot_cards)
 
                 print(f"Your final hand: {player_cards}, final score = {player_score}")
                 print(f"Bot final hand: {bot_cards}, bot final score = {bot_score}")
@@ -68,6 +72,9 @@ def play_game():
                 elif player_score == bot_score:
                     print("Draw")
                     game_over = True
+                elif bot_score > 21:
+                    print("The bot went over! You win!")
+                    game_over = True
                 else:
                     print("You lose!")
                     game_over = True
@@ -76,12 +83,14 @@ def play_game():
                 print(f"Your final hand: {player_cards}, final score = {player_score}")
                 print(f"Bot final hand: {bot_cards}, bot final score = {bot_score}")
                 print("You went over, you lose!")
+                game_over = True
 
-            
-        
+        play_again = input("Would you like to play again? (y/n): ")
+        if play_again == 'y':
+            playing = True
+        else: playing = False
 
-
-        
+      
 
 def check_gameover(score):
     if score > 21:
@@ -90,7 +99,7 @@ def check_gameover(score):
         return False
     
 def check_blackjack(cards):
-    if 11 in cards and 10 in cards:
+    if (11 in cards and 10 in cards) or (10 in cards and 11 in cards):
         return True
     else:
         return False
@@ -118,9 +127,6 @@ def calculate_score(cards):
     for card in cards:
         score += card
     return score
-
-
-
 
 while input("Would you like to play a game of blackjack? (y/n): ") == "y":
     clear()
