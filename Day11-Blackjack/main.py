bb6bd1d57c4b44a77592d5692c4295bf9e287d72
JordanPenaloza <jordan332033@gmail.com>
2024-01-16 12:07:11 -0800

    fixed player score calculattion
    added calculation function
    added cards to array list

--- Day11-Blackjack/main.py
+++ Day11-Blackjack/main.py
@@ -3,20 +3,29 @@ import random
 import os
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+player_cards = []
+bot_cards = []
+
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def play_game():
     print(logo)
-    player_card1 = draw_card()
-    player_card2 = draw_card()
-    print(f"Your cards: [{player_card1}, {player_card2}]")
-
+    player_cards.append(draw_card())
+    player_cards.append(draw_card())
+    player_score = calculate_score(player_cards)
+    print(f"Your cards: {player_cards}, current score = {player_score}")
 
 def draw_card():
     card = cards[random.randint(0,12)]
     return card
+
+def calculate_score(cards):
+    score = 0
+    for card in cards:
+        score += card
+    return score
 
 
 
